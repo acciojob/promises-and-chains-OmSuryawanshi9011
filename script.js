1652f7afedfb5fc354b574a86c79a8a468609905
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-22 11:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // This line prevents the form from submitting normally
+
+    let age = document.getElementById('age').value;
+    let name = document.getElementById('name').value;
+
+    let promise = new Promise(function(resolve, reject) {
+        if(age >= 18) {
+            setTimeout(() => resolve(`Welcome, ${name}. You can vote.`), 4000);
+        } else {
+            reject(`Oh sorry ${name}. You aren't old enough.`);
+        }
+    });
+
+    promise.then(
+        result => alert(result), // shows "Welcome, <name>. You can vote." after 4 sec if resolved
+        error => alert(error) // shows "Oh sorry <name>. You aren't old enough." if rejected
+    );
+});
